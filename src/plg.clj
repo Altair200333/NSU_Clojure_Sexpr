c005f560b836c8dcafdcf8564cc3e4597d520874
Neo <m.petrenko@g.nsu.ru>
2023-01-20 17:24:40 +0700

    feat: detect matches

--- src/plg.clj
+++ src/plg.clj
@@ -1,4 +1,5 @@
 (ns plg)
+ (require '[clojure.string :as str])
 
 ;; Testing stuff
 
@@ -54,8 +55,11 @@
 
 
 (defn name-is [expr name]
-  {:doc "Check if tag name is equal to @name"}
-  (= (tag-name expr) name))
+  {:doc "Check if tag name is equal to @name
+         typeof name: [string, keyword]"}
+  (if (keyword? name)
+    (= (tag-name expr) name)
+    (= (tag-name expr) (keyword name))))
 
 (defn value-is [expr value] 
   {:doc "Check if (first) argument of expr is equal to value"}
@@ -70,6 +74,7 @@
     (some (fn [x] (= x value)) values)))
 
 (name-is (tag :div "aa") :div)
+(name-is (tag :div "aa") "div")
 (name-is (tag :div "aa") :br)
 
 (value-is (tag :div "x") "x")
@@ -89,17 +94,71 @@
             (tag :student "name3")
             (tag :student "name4"))))
 
+(def use-list-sample
+  (list 
+   (tag :div "empty") 
+   (tag :students 
+        (tag :student "name4"))
+   (tag :div "element")
+   (tag :div "sample")
+   (tag :br)))
+
 (tag-args use-sample)
 (tag-args (tag :key))
 
+(str/split "path/d/te" #"/")
+(re-matches #"(a+)(b+)(\d+)" "abb234")
 
+(def dict-data {:name "name" :tag value-is})
+(dict-data :name)
 
+;; list of tag predicates (...)
+;; {:tag "name of tag" :matcher *condition on value of tag*}
 
+(def match-all-q {:tag "*"})
+(def match-div-q {:tag "div"})
+(def match-br-q {:tag "br"})
 
+(defn match-query [expr query]
+  (let [name (query :tag)]
+    (if (= name "*")
+      true
+      (name-is expr name))))
 
+(match-query (tag :div "value") match-div-q)
+(match-query (tag :div "value") match-br-q)
+(match-query (tag :div "value") match-all-q)
 
+(defn turn-into-list [expr]
+  {:doc "Make sure expr is list so it's iterable"}
+  (if (tag? expr)
+    (list expr)
+    (if (list? expr)
+      expr
+      (list expr))))
+(turn-into-list (list (tag :name "value") "12"))
 
+(defn query-matching-expressions [expr query]
+  (loop [list-exprs (turn-into-list expr) results []] 
+    (if (empty? list-exprs) 
+      results
+      (let [first-expr (first list-exprs)]
+        (if (match-query first-expr query)
+          (recur (rest list-exprs) (conj results first-expr))
+          (recur (rest list-exprs) results))))))
 
+(match-query (first (turn-into-list use-list-sample)) match-div-q)
+
+(query-matching-expressions use-list-sample match-div-q)
+
+(defn find-query [expr query]
+  (let [list-exprs (turn-into-list expr) queries (turn-into-list query)]
+    (loop [tags list-exprs results []]
+      (if (empty? tags)
+        results
+        (recur (rest tags) (conj results (first tags)) )))))
+
+(find-query use-sample match-div-q)
 
 
 
